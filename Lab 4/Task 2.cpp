243238d2f07e15c46025fce93ea095b9ba179ba2
Abdul Majid <mkabdulmajid2006@gmail.com>
2025-09-15 22:27:07 +0500

    Create Task 2.cpp

---
+++ Lab 4/Task 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Unsorted array: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
