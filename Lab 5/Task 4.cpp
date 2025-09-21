ddbe2a3cd53e81fb42c9faeabae0b337a8f800ee
Abdul Majid <mkabdulmajid2006@gmail.com>
2025-09-21 21:46:28 +0500

    Create Task 4.cpp

---
+++ Lab 5/Task 4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int ackermann(int m, int n) 
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {    
+        return ackermann(m - 1, 1);
+    }
+    else
+    {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}
+
+int main() 
+{
+    int m, n;
+    cout << "Enter value of m: ";
+    cin >> m;
+    cout << "Enter value of n: ";
+    cin >> n;
+    cout << "Ackermann(" << m << "," << n << ") = " << ackermann(m, n) << endl;
+    return 0;
+}
