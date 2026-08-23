d508e688b39557caa94af7a9da531ccc78e77ea2
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-23 18:28:14 +0500

    Add tail and non-tail recursive sum functions
    Implemented sumTail function for tail recursion and started sumNonTail function for non-tail recursion.

--- Lab 5/Task 3.cpp
+++ Lab 5/Task 3.cpp
@@ -1,3 +1,12 @@
+/* Task#3:
+1. Implement a C++ function sumTail(int n, int total) that calculates the sum
+of numbers from 1 to n using tail recursion. The recursive call should be the last
+operation in the function.
+2. Implement another function sumNonTail(int n) that calculates the same sum
+using non-tail recursion. This function should perform an operation (e.g., addition) after
+the recursive call returns. Compare the two implementations and explain the difference
+in their call stacks. */
+
 #include <iostream>
 using namespace std;
 
