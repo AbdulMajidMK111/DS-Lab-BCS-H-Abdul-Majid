f4713b77e7b9c4c3d80357a57022cf3c27595536
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-23 18:27:10 +0500

    Add recursive factorial function
    Implement recursive function to calculate factorial.

--- Lab 5/Task 1.cpp
+++ Lab 5/Task 1.cpp
@@ -1,3 +1,8 @@
+/* Task#1:
+Write a recursive C++ function calculateFactorial(int n) that computes the factorial
+of a given integer n . The function should have a base condition to stop the recursion when n is
+0 or 1, and should call itself with a smaller value of n (e.g., n-1) to solve the problem. */
+
 #include <iostream>
 using namespace std;
 
