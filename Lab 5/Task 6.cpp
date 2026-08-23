7a3dddf124d0255605c5995824e0379fa0632178
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-23 18:29:53 +0500

    Modify maze solver to support four-direction movement
    Enhanced the 'Rat in a Maze' algorithm to allow movement in four directions and maintain backtracking functionality.

--- Lab 5/Task 6.cpp
+++ Lab 5/Task 6.cpp
@@ -1,3 +1,9 @@
+/* Task#6:
+Modify the provided "Rat in a Maze" code to handle a more complex version of the problem.
+The rat should now be able to move in four directions (up, down, left, and right) instead of just
+two. Your updated code should still use backtracking to find a valid path from the source (0,0) to
+the destination (N-1, N-1) while avoiding dead ends. */
+
 #include <iostream>
 using namespace std;
 
