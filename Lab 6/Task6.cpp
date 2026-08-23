264d8154a2d96185cbfff788d471ad5c5248c12c
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-23 18:36:56 +0500

    Add task description for infix to prefix conversion
    Added a comment block outlining the task requirements for infix to prefix conversion using a stack.

--- Lab 6/Task6.cpp
+++ Lab 6/Task6.cpp
@@ -1,3 +1,9 @@
+/* Task#6:
+Infix to Prefix Conversion using Stack:
+Write a program to convert an infix expression into prefix form using stack rules.
+1. Include operator precedence and associativity handling.
+2. Evaluate the prefix expression as well. */
+
 #include <iostream>
 #include <string>
 using namespace std;
