dce5b4615ae24f25709456c7c72c334e71246df7
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-23 18:11:40 +0500

    Add task description for student marks program
    Added a comment block outlining the task requirements for handling student marks using a jagged array.

--- Lab 2/Task3.cpp
+++ Lab 2/Task3.cpp
@@ -1,3 +1,10 @@
+/* Task#3:
+Design a program to handle marks of students in different courses using a jagged array
+(since each student is enrolled in a different number of courses).
+=> Input the number of courses each student takes.
+=> Dynamically allocate memory for each student accordingly.
+=> Calculate and display each student’s average marks. */
+
 #include <iostream>
 using namespace std;
 
