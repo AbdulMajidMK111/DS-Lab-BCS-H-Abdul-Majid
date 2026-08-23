20f06dbb9c55b34e868fd5417f06f309f3a9c230
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-23 18:31:51 +0500

    Add task description for stack implementation
    Added a comment block outlining the task requirements for a basic stack implementation using arrays.

--- Lab 6/Task 1.cpp
+++ Lab 6/Task 1.cpp
@@ -1,3 +1,11 @@
+/* Task#1:
+Basic Stack Implementation:
+Implement a stack using arrays. Support the following operations:
+1. Push an element
+2. Pop an element
+3. Peek (top element)
+4. Display stack contents */
+
 #include <iostream>
 using namespace std;
 
